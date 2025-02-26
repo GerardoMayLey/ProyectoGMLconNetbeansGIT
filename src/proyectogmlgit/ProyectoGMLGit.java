9039d95511425a590bce3984212fb8a08524413f
Gerardo <gery1814@gmail.com>
2025-02-26 12:47:02 +0100

    Primer commit realizado por GML

---
+++ src/proyectogmlgit/ProyectoGMLGit.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectogmlgit;
+
+/**
+ *
+ * @author gerardo
+ */
+public class ProyectoGMLGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+int x=2;
+int y=3;
+
+int resultado=x+y;
+
+
+    }
+    
+}
